bb666a16649518192d710879e5687e4e1e470635
RounakAgarwal2275 <rounak16hotmail@gmail.com>
2023-01-11 23:32:09 +0530

    flipkart

---
+++ flipkart/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Car from './ClassComponent';
+import Hello from './FunctionalConponent';
+function App() {
+  return (
+    <div className="App">
+    <Car/>
+    <Hello/>
+    </div>
+  );
+}
+
+export default App;
---
+++ flipkart/src/FunctionalConponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Hello(){
+    return (
+        <div className="mainComponent">
+        <h1>Hello from functional component</h1>;
+        </div>
+    );
+}
+
+export default Hello;
